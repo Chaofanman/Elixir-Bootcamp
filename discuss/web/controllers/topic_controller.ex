e4bb0b32a685b05042eb4b2c84bba853854e9b59
Gabriel Patron <gchase.patron@gmail.com>
2016-12-20 19:00:52 +0800

    List of topics done at index

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -3,6 +3,11 @@ defmodule Discuss.TopicController do
 
 	alias Discuss.Topic
 
+	def index(conn, _params) do
+		topics = Repo.all(Topic)
+		render conn, "index.html", topics: topics
+	end
+
 	def new(conn, _params) do
 		changeset = Topic.changeset(%Topic{}, %{})
 
@@ -17,5 +22,5 @@ defmodule Discuss.TopicController do
 			{:error, changeset} -> 
 				render conn, "new.html", changeset: changeset
 		end
-	end
+	end	
 end
